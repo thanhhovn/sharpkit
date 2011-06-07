c77e29472ea9571e8f583fbdd34fc542905860ab
danelkhen <DanelKhen@gmail.com>
2011-06-07 10:09:20 +0000

    v4.05.1000

--- SharpKit.AspNetAjax/Properties/AssemblyInfo.cs
+++ SharpKit.AspNetAjax/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.5.0.0")]
-[assembly: AssemblyFileVersion("4.05.0000")]
+[assembly: AssemblyFileVersion("4.05.1000")]
