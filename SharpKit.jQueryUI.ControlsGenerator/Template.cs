fe8c8378e0efcc87b65f4c66eb69f70945af9cd1
danelkhen <DanelKhen@gmail.com>
2011-06-25 10:54:08 +0000

    Create jQueryUI.Controls library

---
+++ SharpKit.jQueryUI.ControlsGenerator/Template.cs
@@ -0,0 +1,35 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using SharpKit.JavaScript;
+//using SharpKit.jQuery;
+//using SharpKit.jQuery.UI;
+//namespace WebDesigner.Client
+//{
+//    [JsType(JsMode.Prototype)]
+//    public class jSlider : jQueryContext
+//    {
+//        public jSlider(JsString selector, SliderOptions options)
+//        {
+//            Selector = J(selector);
+//            Selector.slider(options);
+//        }
+
+//        public JsNumber value
+//        {
+//            get { return Get("value").As<JsNumber>(); }
+//            set { Set("value", value); }
+//        }
+
+//        object Get(JsString option)
+//        {
+//            return Selector.slider(option);
+//        }
+//        void Set(JsString option, object value)
+//        {
+//            Selector.slider(option, value);
+//        }
+//        jQuery Selector;
+//    }
+//}
