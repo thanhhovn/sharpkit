cbf9fad5367a6286009fc7d7d1161369af8443f4
danelkhen@gmail.com <danelkhen@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2011-07-20 15:57:01 +0000

    Added missing methods Added InvalidMethods.txt

---
+++ trunk/SharpKit.ExtJs-4.0.2/ExtJsContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpKit.JavaScript;
+
+namespace Ext
+{
+    [JsType(JsMode.Global, Export=false)]
+    public class ExtJsContext : JsContext
+    {
+        public static ExtGlobal Ext { get; set; }
+    }
+}
