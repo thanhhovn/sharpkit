1b3e1a14f4ba5f9847955b2f640fb52fc876787c
danelkhen <DanelKhen@gmail.com>
2011-08-06 15:53:31 +0000

    Added JsContext.String(object obj)

--- SharpKit.JavaScript/javascript-1.5.cs
+++ SharpKit.JavaScript/javascript-1.5.cs
@@ -130,32 +130,32 @@ namespace SharpKit.JavaScript
     ///<summary>
     ///Specifies the export and interopability mode of a C# type in JavaScript
     ///</summary>
-    public enum JsMode 
-    { 
+    public enum JsMode
+    {
         /// <summary>
         /// Specifies a global function export mode, in which only static members are allowed,
         /// static methods become global functions
         /// static fields become global variables
         /// static constrctor becomes global code
         /// </summary>
-        Global, 
+        Global,
         /// <summary>
         /// Specifies a prototype object export mode, in which a single constructor is allowed, and both static and instance members.
         /// constructor becomes a constructor function
         /// instance members become the equivalent members on the constructor function's prototype.
         /// static members become members on the constructor function itself.
         /// </summary>
-        Prototype, 
+        Prototype,
         /// <summary>
         /// Specifies a .NET style class, in which all C# elements are supported,
         /// this mode requires JsClr library to be included on the client at runtime.
         /// </summary>
-        Clr, 
+        Clr,
         /// <summary>
         /// Specifies an invisible unexported json type, this class will not be exported, 
         /// instantiation and usage of classes in this mode, will be exported to simple json elements.
         /// </summary>
-        Json 
+        Json
     }
 
     #endregion
@@ -703,6 +703,12 @@ namespace SharpKit.JavaScript
         ///<param name="s">A JsString that contains a floating-point number.</param>
         ///<returns>A numerical value equal to the number contained in numString. If no prefix of numString can be successfully parsed into a floating-point number, NaN (not a number) is returned.</returns>
         public static float parseFloat(JsString s) { return default(float); }
+        ///<summary>
+        ///Returns an string converted from a value.
+        ///</summary>
+        ///<param name="value">A value to convert into a string.</param>
+        ///<returns>An string value</returns>
+        public static string String(object value) { return null; }
     }
     #endregion
     #region JsDate
