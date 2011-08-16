695f5847bd8e80944a22a91822d1af96522f9974
danelkhen <DanelKhen@gmail.com>
2011-08-16 07:25:48 +0000

    Remove comma at the ent of PropertyIs function

--- SharpKit.JsClr-4.1.0/res/System.js
+++ SharpKit.JsClr-4.1.0/res/System.js
@@ -3499,7 +3499,7 @@ JsTypes.push({fullname:"VM",  definition:
             return TypeIs(propertyType, type);
         }
         return false;
-    },
+    }
 }
 });
 JsTypes.push({fullname:"SharpKit.DataModel.NotifyCollectionChangedEventArgs", baseTypeName:"System.Object", definition:
