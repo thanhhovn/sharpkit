d34fb01fa49c827d0d63b0323b08bd858e8c00fe
DanelKhen@gmail.com <DanelKhen@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2011-09-29 15:02:56 +0000

    Trasnform WebSocket into class

---
+++ trunk/SharpKit.W3C.DOM/Constructors.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpKit.JavaScript;
+using org.w3c.dom.html;
+
+namespace org.w3c.dom.websocket
+{
+    public class WebSocket
+    {
+        public WebSocket(string url) { }
+        public WebSocket(string url, JsString protocol) { }
+        public JsString uRL { get; private set; }
+        public const short CONNECTING = 0;
+        public const short OPEN = 1;
+        public const short CLOSED = 2;
+        public short readyState { get; private set; }
+        public int bufferedAmount { get; private set; }
+        public Function onopen { get; set; }
+        public Function onmessage { get; set; }
+        public Function onclose { get; set; }
+        public bool send(string data) { return false; }
+        public void close() { }
+    }
+}
