6fc971bd1cfa3b655ff7a79e537daf221a2fa7c3
DanelKhen@gmail.com <DanelKhen@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2011-10-16 09:41:30 +0000

    Fix jsclr4.1.0 output folders

--- trunk/SharpKit.JsClr-4.1.0/Properties/AssemblyInfo.cs
+++ trunk/SharpKit.JsClr-4.1.0/Properties/AssemblyInfo.cs
@@ -34,7 +34,7 @@ using SharpKit.JavaScript;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("4.21.0000")]
+[assembly: AssemblyFileVersion("4.21.5000")]
 
 
 [assembly: JsMergedFile(Filename = "res/System.js", Sources = new string[]
