35fc97979e0737455afe0f4462de4eb34146e1b8
DanelKhen@gmail.com <DanelKhen@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2011-10-28 12:25:35 +0000

    Create 'latest version' projects of SharpKit interface assemblies - these will contain the latest version of a certain library

--- trunk/SharpKit.Html4/Properties/AssemblyInfo.cs
+++ trunk/SharpKit.Html4/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.0.1.0")]
-[assembly: AssemblyFileVersion("4.21.7000")]
+[assembly: AssemblyFileVersion("4.21.8000")]
