28d6d1c5ad8d4aa3a981dd3833f24825d680b38e
DanelKhen@gmail.com <DanelKhen@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2011-11-29 19:16:06 +0000

    Added SharpKit.Html project - HTML5 (+4) contributed by Sebastian Loncar

---
+++ trunk/SharpKit.Html/SharpKit.cs
@@ -0,0 +1,10 @@
+using SharpKit.JavaScript;
+//Default JsType attribute
+[assembly: JsType(JsMode.Prototype, PropertiesAsFields = true, Export = false, OmitCasts=true)]
+
+/// <summary>
+/// Latest HTML interfaces, created according to w3c specifications, including HTML5, CSS3, SVG, WebGL, WebDatabase and more.
+/// </summary>
+class AssemblyDoc
+{
+}
