5be934d2e73e4abcf8fd964ac40cae13e3a49fd5
DanelKhen@gmail.com <DanelKhen@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2011-12-21 13:16:33 +0000

    Added jQuery.query (query string plugin) library support

--- trunk/SharpKit.Html.Linq/Properties/AssemblyInfo.cs
+++ trunk/SharpKit.Html.Linq/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using SharpKit.JavaScript;
@@ -34,5 +34,5 @@ using SharpKit.JavaScript;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("4.23.5000")]
 [assembly: JsMergedFile(Filename="res/SharpKit.Html.Linq.min.js", Sources=new string[]{"res/SharpKit.Html.Linq.js"}, Minify=true)]
