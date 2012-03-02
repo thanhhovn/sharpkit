c6863082eeec1490455394776eb04d60439dc6eb
DanelKhen@gmail.com <DanelKhen@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2012-03-02 17:16:24 +0000

    Initial support for IEqualityComparer<T> and StringComparer.InvariantCultureIgnoreCase

--- trunk/SharpKit.JsClr/System/Collections/Generic/Interfaces.cs
+++ trunk/SharpKit.JsClr/System/Collections/Generic/Interfaces.cs
@@ -148,4 +148,63 @@ namespace SharpKit.JavaScript.Private
     }
 
 
+    /// <summary>Defines methods to support the comparison of objects for equality.</summary>
+    /// <typeparam name="T">The type of objects to compare.This type parameter is contravariant. That is, you can use either the type you specified or any type that is less derived. For more information about covariance and contravariance, see Covariance and Contravariance in Generics.</typeparam>
+    [JsType(Name="System.Collection.Generic.IEqualityComparer", Filename = "~/res/System.js")]
+    public interface JsImplIEqualityComparer<in T>
+    {
+        /// <summary>Determines whether the specified objects are equal.</summary>
+        /// <returns>true if the specified objects are equal; otherwise, false.</returns>
+        /// <param name="x">The first object of type <paramref name="T" /> to compare.</param>
+        /// <param name="y">The second object of type <paramref name="T" /> to compare.</param>
+        bool Equals(T x, T y);
+        /// <summary>Returns a hash code for the specified object.</summary>
+        /// <returns>A hash code for the specified object.</returns>
+        /// <param name="obj">The <see cref="T:System.Object" /> for which a hash code is to be returned.</param>
+        /// <exception cref="T:System.ArgumentNullException">The type of <paramref name="obj" /> is a reference type and <paramref name="obj" /> is null.</exception>
+        int GetHashCode(T obj);
+    }
+
+
+    [JsType(Name="System.StringComparer", Filename = "~/res/System.Collections.js")]
+    public abstract class StringComparer : JsImplIEqualityComparer<string>
+    {
+        static StringComparer _InvariantCultureIgnoreCase;
+        public static StringComparer InvariantCultureIgnoreCase
+        {
+            get
+            {
+                if (_InvariantCultureIgnoreCase == null)
+                    _InvariantCultureIgnoreCase = new StringComparer_InvariantCultureIgnoreCase();
+                return _InvariantCultureIgnoreCase;
+            }
+        }
+        #region JsImplIEqualityComparer<string> Members
+        [JsMethod(Name = "Equals$$T$$T", NativeOverloads = true)]
+        public abstract bool Equals(string x, string y);
+
+        [JsMethod(Name = "GetHashCode$$T", NativeOverloads=true)]
+        public abstract int GetHashCode(string obj);
+
+        #endregion
+    }
+
+    [JsType(Filename = "~/res/System.Collections.js")]
+    class StringComparer_InvariantCultureIgnoreCase :StringComparer
+    {
+        public override bool Equals(string x, string y)
+        {
+            if (x == y)
+                return true;
+            if (x == null || y == null)
+                return false;
+            return x.As<JsString>().toLowerCase() == y.As<JsString>().toLowerCase();
+        }
+        public override int GetHashCode(string obj)
+        {
+            if (obj == null)
+                return "null".As<int>();
+            return obj.As<JsString>().toLowerCase().As<int>();
+        }
+    }
 }
