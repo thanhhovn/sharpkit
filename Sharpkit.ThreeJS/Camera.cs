c4c97aa728b6d74ae8b3d8ac76210af72a107b9c
reiniervvliet <reiniervvliet@gmail.com>
2012-04-21 21:05:56 +0000

    - initial commit of Three.js bindings for SharpKit. Small example project included.   Still needs a lot of classes, properties and methods to be added

---
+++ Sharpkit.ThreeJS/Camera.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using SharpKit.Html4;
+using SharpKit.JavaScript;
+
+namespace THREE
+{
+    [JsType(JsMode.Prototype, Export=false)] 
+    public class Camera : Object3D 
+    { 
+        public Camera() {}
+        public Matrix4  matrixWorldInverse  {get;set;} 
+        public Matrix4  projectionMatrix  {get;set;} 
+        public Matrix4  projectionMatrixInverse  {get;set;} 
+        public void lookAt (Vector3 target) {}
+    }
+}
---
+++ Sharpkit.ThreeJS/PerspectiveCamera.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using SharpKit.Html4;
+using SharpKit.JavaScript;
+
+namespace THREE
+{
+    [JsType(JsMode.Prototype, Export = false)]
+    public class PerspectiveCamera : Camera
+    {
+        public PerspectiveCamera(JsNumber fov, JsNumber aspect, JsNumber near, JsNumber far) { }
+
+        public void setLens(JsNumber focalLength, JsNumber frameHeight) { }
+        public void setViewOffset(JsNumber fullWidth, JsNumber fullHeight, JsNumber x, JsNumber y, JsNumber width, JsNumber height) { }
+        public void updateProjectionMatrix() { }
+    }
+}
