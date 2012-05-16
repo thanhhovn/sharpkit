f0f7cc59f3510daae786c87c1cb7b375d51611a4
danelkhen <DanelKhen@gmail.com>
2012-05-16 17:11:28 +0000

    Added empty project for Backbone.js

---
+++ SharpKit.BackboneJs/Backbone.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpKit.JavaScript;
+
+namespace SharpKit.BackboneJs
+{
+    [JsType(JsMode.Prototype)]
+    public partial class Backbone
+    {
+    }
+}
