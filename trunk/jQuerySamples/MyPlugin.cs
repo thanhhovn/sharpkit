0ede4ffdcd4d2e3f0aa254f7011d3fce8615cba6
DanelKhen@gmail.com <DanelKhen@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2012-06-01 18:06:04 +0000

    Added jQuery plugin sample

---
+++ trunk/jQuerySamples/MyPlugin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SharpKit.jQuery;
+using SharpKit.JavaScript;
+
+namespace jQuerySamples
+{
+    [JsType(JsMode.Prototype, Name = "jQuery.fn", OmitInheritance = true, OmitDefaultConstructor = true)]
+    class MyPlugin : jQueryPlugin
+    {
+        public static jQuery myPlugin()
+        {
+            @this.append("Hello world, this is my SharpKit jQuery plugin!<br/>");
+            return @this;
+        }
+    }
+
+    [JsType(JsMode.Global, Export=false)]
+    class jQueryPlugin
+    {
+        public static jQuery @this { get; set; }
+    }
+
+    [JsType(JsMode.Prototype, Export=false)]
+    public static class MyPluginExtension
+    {
+        [JsMethod(ExtensionImplementedInInstance=true)]
+        public static jQuery myPlugin(this jQuery j) { return null; }
+    }
+}
