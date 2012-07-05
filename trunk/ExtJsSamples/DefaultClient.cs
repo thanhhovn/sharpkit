75888af2e2817d41f2decec9ba1000eb5afa0a51
DanelKhen@gmail.com <DanelKhen@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2012-07-05 11:10:49 +0000

    Add ClrModeSamples, ExtJsSamples, jQuerySamples, NodeJsApiGenerator Initial NodeJs API (generated - not to be used yet)

---
+++ trunk/ExtJsSamples/DefaultClient.cs
@@ -0,0 +1,19 @@
+﻿using SharpKit.JavaScript;
+using SharpKit.Html4;
+using SharpKit.jQuery;
+using Ext.window;
+using Ext;
+
+namespace ExtJsSamples
+{
+    [JsType(JsMode.Global)]
+    public class DefaultClient
+    {
+        static void Main()
+        {
+            var win = new Window(new WindowConfig { width = 300, height = 200 });
+            win.show();
+            ExtContext.create<Window>(new WindowConfig { width = 100, height = 100 }).show();
+        }
+    }
+}
--- trunk/SharpKit.JavaScript.NoClr.Samples/Properties/AssemblyInfo.cs
+++ trunk/SharpKit.JavaScript.NoClr.Samples/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using SharpKit.JavaScript;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("4.27.9000")]
+[assembly: AssemblyFileVersion("4.28.3000")]
