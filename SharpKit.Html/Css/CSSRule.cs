86e8ab9217878d642b41470cc40c8350ea9611ce
sebastian.loncar <sebastian.loncar@googlemail.com>
2012-07-16 23:09:09 +0000

    corrected CSSStyle IDL API

--- SharpKit.Html/Css/CSSRule.cs
+++ SharpKit.Html/Css/CSSRule.cs
@@ -3,23 +3,33 @@
 namespace SharpKit.Html
 {
 
-using SharpKit.JavaScript;
+	using SharpKit.JavaScript;
 
-[JsType(JsMode.Prototype, Export = false, PropertiesAsFields = true, OmitCasts = true)]
-public partial class CSSRule
-{
+	[JsType(JsMode.Prototype, Export = false, PropertiesAsFields = true, OmitCasts = true, Name = "CSSRule")]
+	public partial class CSSRule
+	{
 
-	// CSSRule
-	public static short STYLE_RULE = 1;
-	public static short IMPORT_RULE = 3;
-	public static short MEDIA_RULE = 4;
-	public static short FONT_FACE_RULE = 5;
-	public static short PAGE_RULE = 6;
-	public static short NAMESPACE_RULE = 10;
-	public short type {get; private set; }
-	public JsString cssText {get; set; }
-	public CSSRule parentRule {get; private set; }
-	public CSSStyleSheet parentStyleSheet {get; private set; }
-}
+		// CSSRule
+		public static short STYLE_RULE = 1;
+		public static short IMPORT_RULE = 3;
+		public static short MEDIA_RULE = 4;
+		public static short FONT_FACE_RULE = 5;
+		public static short PAGE_RULE = 6;
+		public static short NAMESPACE_RULE = 10;
+		public short type { get; private set; }
+		public JsString cssText { get; set; }
+		public CSSRule parentRule { get; private set; }
+		public CSSStyleSheet parentStyleSheet { get; private set; }
+	}
+
+	[JsType(JsMode.Prototype, Export = false, PropertiesAsFields = true, OmitCasts = true)]
+	public partial class CSSRuleList
+	{
+
+		public int length { get; private set; }
+		[JsProperty(NativeIndexer = true)]
+		public CSSStyleRule this[int index] { get { return default(CSSStyleRule); } }
+
+	}
 
 }
