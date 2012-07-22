2600ce082b86cb3e587250ffaeeef47624268097
danelkhen <DanelKhen@gmail.com>
2012-07-22 07:38:15 +0000

    Begin support for KineticJs

---
+++ SharpKit.KineticJs/kinetic-v3.10.3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpKit.KineticJs
+{
+    public class Node
+    {
+        /// <summary>
+        /// Node constructor.  Nodes are entities that can be transformed, layered,and have events bound to them. They are the building blocks of a KineticJSapplication
+        /// </summary>
+        /// <param name="config"></param>
+        public Node(object config) { }
+
+        /// <summary>
+        /// clone node
+        /// </summary>
+        /// <param name="config">used to override cloned attrs</param>
+        /// <returns></returns>
+        public Node clone(object config) { return null; }
+
+    }
+
+}
