78f18504b227b379ab302ee7e906d871ab1d5810
DanelKhen@gmail.com <DanelKhen@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2012-08-20 14:39:34 +0000

    Move THREE to SharpKit.Three project

---
+++ trunk/SharpKit.Three/MaterialOptions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using SharpKit.Html4;
+using SharpKit.JavaScript;
+
+namespace THREE
+{
+    public enum BlendingMode
+    {
+        NoBlending = 0,
+        NormalBlending = 1,
+        AdditiveBlending = 2,
+        SubtractiveBlending = 3,
+        MultiplyBlending = 4,
+        AdditiveAlphaBlending = 5
+    };
+
+    [JsType(JsMode.Prototype, Export = false)]
+    public class MaterialOptions
+    {
+        public JsNumber opacity { get; set; }
+        public JsBoolean transparent { get; set; }
+
+        public BlendingMode blending { get; set; }
+        public JsBoolean depthTest { get; set; }
+        public JsBoolean depthWrite { get; set; }
+
+        public JsBoolean polygonOffset { get; set; }
+        public JsNumber polygonOffsetFactor { get; set; }
+        public JsNumber polygonOffsetUnits { get; set; }
+        public JsNumber alphaTest { get; set; }
+        public JsNumber overdraw { get; set; }
+
+    }
+}
---
+++ trunk/SharpKit.Three/WebGLRendererOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using SharpKit.Html4;
+using SharpKit.JavaScript;
+
+namespace THREE
+{
+    [JsType(JsMode.Json, Export = false)]
+    public class WebGLRendererOptions
+    {
+
+        //todo: add all other params
+        public JsNumber maxLights { get; set; }
+    }
+}
