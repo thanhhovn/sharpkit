a0dc43d9d2e1bb5d362f2b2ef9606c6734a6f891
DanelKhen@gmail.com <DanelKhen@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2012-08-20 16:14:27 +0000

    Add unit tests, write file name being compared

---
+++ trunk/tests/CoreTests/Class2.cs
@@ -0,0 +1,14 @@
+﻿using SharpKit.JavaScript;
+
+namespace SharpKitWebApp5
+{
+    [JsType]
+    public class Class2
+    {
+        void foo()
+        {
+            var x = new int[7, 8];
+            x[4, 5] = 9;
+        }
+    }
+}
