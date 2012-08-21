da8ccc2a03ff08ac4acbdb3f0c1bbe139d2ccc1a
trini16k <trini16k@gmail.com>
2012-08-21 08:11:22 +0000

    Begin google chrome extensions api support

--- SharpKit.Google.Chrome/chrome.cs
+++ SharpKit.Google.Chrome/chrome.cs
@@ -2,7 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SharpKit.JavaScript;
 
 namespace SharpKit.Google.Chrome
 {
+
+
+    
+    public class ChromeEvent<T>
+    {
+        public void addEventListener(JsAction<T> action) { }
+    }
 }
