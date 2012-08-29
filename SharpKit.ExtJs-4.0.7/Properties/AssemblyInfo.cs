a6d9e14e0437aa56544e55cb60fff56c9045acb8
jeronimomilea <jeronimomilea@gmail.com>
2012-08-29 17:07:14 +0000

    Added latest legacy version of ExtJs 4.0.7

---
+++ SharpKit.ExtJs-4.0.7/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle( "SharpKit.ExtJs" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "SharpKit.ExtJs" )]
+[assembly: AssemblyCopyright( "Copyright ©  2012" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+[assembly: ComVisible( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid( "80EC85D8-031E-42B3-BED4-04DADB89D7B3" )]
+
+//Generated from ExtJs 4.0.2 help files
+[assembly: AssemblyVersion( "4.0.7.0" )]
+[assembly: AssemblyFileVersion( "4.0.7.1" )]
+
+
+
+/// <summary>
+/// SharpKit interfaces for Ext JS 4.
+/// Ext JS 4 is a pure JavaScript application framework that works everywhere from IE6 to latest Chrome. It enables you to create the best cross-platform applications using nothing but a browser, and has a phenomenal API.
+/// </summary>
+class AssemblyDoc
+{
+}
