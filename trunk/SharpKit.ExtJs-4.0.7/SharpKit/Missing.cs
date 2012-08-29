a983d25c5931f2c8e836310cb7ddd9ec2ab14e7a
jeronimomilea@gmail.com <jeronimomilea@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2012-08-29 17:07:14 +0000

    Added latest legacy version of ExtJs 4.0.7

---
+++ trunk/SharpKit.ExtJs-4.0.7/SharpKit/Missing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpKit.JavaScript;
+
+namespace Ext.panel
+{
+    public partial class TableConfig
+    {
+        /// <summary>
+        /// The Store the grid should use as its data source (required).
+        /// </summary>
+        public Ext.data.Store store { get; set; }
+        /// <summary>
+        /// A config object that will be applied to the grid's UI view. Any of the config options available for Ext.view.Table can be specified here. This option is ignored if view is specified.
+        /// </summary>
+        public object viewConfig { get; set; }
+    }
+
+}
