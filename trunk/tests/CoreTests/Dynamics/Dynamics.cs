2902e146313b450df7cedad33af5446f3be1b66e
DanelKhen@gmail.com <DanelKhen@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2012-08-30 07:29:41 +0000

    Add Michael's reported bugs into unit-tests (fixed)

--- trunk/tests/CoreTests/Dynamics/Dynamics.cs
+++ trunk/tests/CoreTests/Dynamics/Dynamics.cs
@@ -9,6 +9,9 @@ namespace CoreTests.Dynamics
     [JsType(JsMode.Prototype)]
     class Dynamics
     {
+        private JsArray data;
+        private int selectedIndex;
+        private object HOW_LONG;
         void TestMethod() { dynamic d; }
 
 
@@ -24,6 +27,43 @@ namespace CoreTests.Dynamics
         public static bool ContainsIgnoreCase(string str, string other)
         {
             return (str.As<dynamic>().ToLower().IndexOf(other.As<dynamic>().ToLower())) > -1;
+        }
+
+
+        public void selectByURL(JsString url)
+        {
+            JsArray dataRef = data;
+            dynamic item;
+
+            for (int i = 0; i < dataRef.length; i++)
+            {
+                item = dataRef[i];
+
+                if (item.url == url)
+                {
+                    if (selectedIndex != i)
+                    {
+                        selectedIndex = i;
+                    }
+                    break;
+                }
+            }
+
+        }
+
+
+        void foo()
+        {
+            dynamic arrivingViewProps = new JsObject();
+            arrivingViewProps.left = 0;
+            arrivingViewProps.leaveTransforms = true;
+ 
+            animate(arrivingViewProps, HOW_LONG);
+    }
+
+        private void animate(dynamic arrivingViewProps, object HOW_LONG)
+        {
+            throw new NotImplementedException();
         }
 
     }
