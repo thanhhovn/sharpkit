c8e8d1549660dafd326c99b4c26ff58a776c0ca7
lee.khn@gmail.com <lee.khn@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2012-09-23 19:59:20 +0000

    Add KendoUI samples

---
+++ trunk/samples/KendoUISamples/examples/web/dragdrop/Container.cs
@@ -0,0 +1,28 @@
+﻿using SharpKit.JavaScript;
+using SharpKit.KendoUI;
+using SharpKit.Html4;
+using SharpKit.jQuery;
+using SharpKit.KendoUI.Web;
+
+
+namespace KendoUISamples.examples.web.dragdrop
+{
+    [JsType(JsMode.Global)]
+    public class Container
+    {
+        static Container()
+        {
+        }
+        static void OnReady()
+        {
+            //TODO
+            //new jQuery("#draggable-item").
+        }
+    //     $("#draggable-item").kendoDraggable({
+    //    container: $("#draggable-container"),
+    //    hint: function() {
+    //        return $("#draggable-item").clone();
+    //    }
+    //});
+    }
+}
---
+++ trunk/samples/KendoUISamples/examples/web/grid/FromTable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SharpKit.JavaScript;
+using SharpKit.jQuery;
+using SharpKit.KendoUI;
+using SharpKit.KendoUI.Web;
+
+namespace KendoUISamples.examples.web.grid
+{
+    [JsType(JsMode.Global)]
+    public class FromTable
+    {
+        static FromTable()
+        {
+            new jQuery(OnReady);
+        }
+        static void OnReady()
+        {
+            new jQuery("#grid").kendoGrid(new GridConfiguration { height = 250 });
+        }
+          //$(document).ready(function() {
+          //          $("#grid").kendoGrid({
+          //              height: 250
+          //          });
+          //      });
+    }
+}
