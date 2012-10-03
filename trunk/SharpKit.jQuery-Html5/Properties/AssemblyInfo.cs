be00df4a943e8539350fa5a5de125447cc1e6de2
yvan.rodrigues@gmail.com <yvan.rodrigues@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2012-10-03 16:09:10 +0000

    Issue 229: SharpKit.jQuery doesn't play nice with SharpKit.Html Forked SharpKit.jQuery as SharpKit.jQuery-Html5.

---
+++ trunk/SharpKit.jQuery-Html5/Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SharpKit.jQuery")]
+[assembly: AssemblyDescription("This fork of SharpKit.jQuery references SharpKit.Html instead of SharpKit.Html4.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SharpKit.jQuery")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+//[assembly: Guid("760dd665-6ae9-4c65-8ee5-6000ef365efb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.8.0.0")]
+[assembly: AssemblyFileVersion("4.29.9000")]
+/// <summary>
+/// SharpKit interface for jQuery.
+/// jQuery is a fast and concise JavaScript Library that simplifies HTML document traversing, event handling, animating, and Ajax interactions for rapid web development. jQuery is designed to change the way that you write JavaScript.
+/// </summary>
+class AssemblyDoc
+{
+}
