d91f2be2f97021ee07378147e23b91fe34c99033
patrickbowie <patrickbowie@gmail.com>
2012-10-16 03:54:10 +0000

    Added headers for all Geometry types. Added headers for Curves

---
+++ SharpKit.Three/Face3.cs
@@ -0,0 +1,13 @@
+﻿using SharpKit.JavaScript;
+using THREE;
+
+namespace SharpKit.Three
+{
+    [JsType(JsMode.Prototype, Export = false)]
+    public class Face3
+    {
+        public Face3(Vector3 a, Vector3 b, Vector3 c, Vector3 normal, Color color, JsNumber materialIndex) { }
+
+        public Face3 clone() { return null; }
+    }
+}
