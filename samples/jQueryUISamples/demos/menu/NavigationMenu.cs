221d8390576ec0c95427d07ceab2b14c48d37b06
lee.khn <lee.khn@gmail.com>
2012-11-04 08:09:13 +0000

    Add new jQueryUI samples and missing files.

---
+++ samples/jQueryUISamples/demos/menu/NavigationMenu.cs
@@ -0,0 +1,30 @@
+﻿using SharpKit.JavaScript;
+using SharpKit.jQuery;
+using SharpKit.Html4;
+namespace jQueryUISamples.demos.menu
+{
+    [JsType(JsMode.Global)]
+    public class NavigationMenu
+    {
+        static NavigationMenu()
+        {
+            new jQuery(OnReady);
+        }
+
+        static void OnReady()
+        {
+            new jQuery("#menu").menu(new MenuOptions
+            {
+                select = (e, ui) =>
+                {
+                    var link = ui.item.children("a:first");
+                    if (link.attr("target").As<bool>() || e.metaKey.As<bool>() || e.shiftKey || e.ctrlKey)
+                    {
+                        return;
+                    }
+                    HtmlContext.window.location.href = link.attr("href");
+                }
+            });
+        }
+    }
+}
---
+++ samples/jQueryUISamples/demos/spinner/Default.cs
@@ -0,0 +1,46 @@
+﻿using SharpKit.JavaScript;
+using SharpKit.jQuery;
+using SharpKit.Html4;
+
+namespace jQueryUISamples.demos.spinner
+{
+    [JsType(JsMode.Global)]
+    public class Default
+    {
+        //TODO: error in run time, ask dan-el.
+        static Default()
+        {
+            new jQuery(OnReady);
+        }
+
+        static void OnReady()
+        {
+            
+		var spin = new jQuery( "#spinner" ).spinner();
+
+		new jQuery( "#disable" ).click(() => 
+        {
+			if ( spin.spinner( "option", "disabled" ).As<bool>() ) {
+				spin.spinner( "enable" );
+			} else {
+				spin.spinner( "disable" );
+			}
+		});
+		new jQuery( "#destroy" ).click(() => {
+			if ( spin.data( "ui-spinner" ).As<bool>() ) {
+				spin.spinner( "destroy" );
+			} else {
+				spin.spinner();
+			}
+		});
+		new jQuery( "#getvalue" ).click(() => {
+			HtmlContext.window.alert( spin.spinner( "value" ) );
+		});
+		new jQuery( "#setvalue" ).click(() => {
+			spin.spinner( "value", 5 );
+		});
+
+		new jQuery( "button" ).button();
+        }
+    }
+}
