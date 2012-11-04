d54027fbeeeb9a3d89a140030d2a4b44aa192f11
lee.khn <lee.khn@gmail.com>
2012-11-04 16:43:05 +0000

    Add jQueryUI samples.

---
+++ samples/jQueryUISamples/demos/tooltip/Forms.cs
@@ -0,0 +1,23 @@
+﻿using SharpKit.JavaScript;
+using SharpKit.jQuery;
+namespace jQueryUISamples.demos.tooltip
+{
+    [JsType(JsMode.Global)]
+    public class Forms
+    {
+        static Forms()
+        {
+            new jQuery(OnReady);
+        }
+
+        static void OnReady()
+        {
+            var tooltips = new jQuery("[title]").tooltip();
+            new jQuery("<button>")
+                .text("Show help")
+                .button()
+                .click(() => tooltips.tooltip("open"))
+                .insertAfter("form");
+        }
+    }
+}
