698ee7f44cf40fe21d7e19fa557b29ddfed4b100
patrickbowie@gmail.com <patrickbowie@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2012-11-04 18:43:39 +0000

    Added JSONLoader, MeshFaceMaterial header to SharpKit.Three

--- trunk/SharpKit.Three/WebGLRendererOptions.cs
+++ trunk/SharpKit.Three/WebGLRendererOptions.cs
@@ -11,8 +11,16 @@ namespace THREE
     [JsType(JsMode.Json, Export = false)]
     public class WebGLRendererOptions
     {
-
-        //todo: add all other params
+        public JsBoolean antialias { get; set; }
+        public HtmlElement canvas { get; set; }
+        public JsNumber clearColor { get; set; }
+        public JsNumber clearAlpha { get; set; }
         public JsNumber maxLights { get; set; }
+        public JsBoolean stencil { get; set; }
+        public JsBoolean preserveDrawingBuffer { get; set; }
+
+        public JsString precision { get; set; }
+        public JsBoolean alpha { get; set; }
+        public JsBoolean premultipliedAlpha { get; set; }
     }
 }
