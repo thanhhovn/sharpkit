a50d18fee6981eebf915afb81638414610d5720b
lee.khn <lee.khn@gmail.com>
2012-11-06 13:22:53 +0000

    Add new project: SharpKit.Html5Sdk.

---
+++ SharpKit.Html5Sdk/DefaultClient.cs
@@ -0,0 +1,19 @@
+﻿using SharpKit.JavaScript;
+using SharpKit.Html4;
+using SharpKit.jQuery;
+
+namespace SharpKit.Html5Sdk
+{
+    [JsType(JsMode.Global, Filename = "res/Default.js")]
+    public class DefaultClient : jQueryContextBase
+    {
+        static void DefaultClient_Load()
+        {
+            J(document.body).append("Ready<br/>");
+        }
+        static void btnTest_click(HtmlDomEventArgs e)
+        {
+            J(document.body).append("Hello world<br/>");
+        }
+    }
+}
