0ea3a8ab8be77c2185917679ebe456fba47d98ad
lee.khn@gmail.com <lee.khn@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2012-11-06 13:22:53 +0000

    Add new project: SharpKit.Html5Sdk.

---
+++ trunk/SharpKit.Html5Sdk/att-html5-sdk.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SharpKit.JavaScript;
+
+namespace SharpKit.Html5Sdk
+{
+    public class att_html5_sdk
+    {
+        public partial class Provider
+        {
+            public void authorizeApp(AuthorizeAppOptions options) { }
+        }
+        [JsType(JsMode.Json)]
+        public partial class AuthorizeAppOptions
+        {
+            public JsString authScope { get; set; }
+            public JsFunction success { get; set; }
+            public JsFunction failure { get; set; }
+        }
+
+        /// <summary>
+        /// Error Handling:
+        ///In case an exception or an error happens, detailed information on the exception/error is available in the error property of the response
+        /// </summary>
+        [JsType(JsMode.Json)]
+        public partial class ProviderOptions
+        {
+            /// <summary>
+            ///  (Default: "att") 
+            ///  The base uri path to be prepended to all API calls. 
+            /// </summary>
+            public JsString apiBasePath { get; set; }
+            /// <summary>
+            /// (Default: "TL,MOBO,MIM,DC")
+            /// This is the default authorization scope used to authorize transactions that require scope and it is not provided.
+            /// </summary>
+            public JsString authScope { get; set; }
+        }
+    }
+}
