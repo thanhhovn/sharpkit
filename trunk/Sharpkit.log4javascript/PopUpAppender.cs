de28546d9db915dc9c9947cf72de21ce51f9a865
maitredede@gmail.com <maitredede@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2012-11-09 10:35:30 +0000

    Initial log4javascript support (incomplete)

---
+++ trunk/Sharpkit.log4javascript/PopUpAppender.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpKit.JavaScript;
+using SharpKit.Html;
+
+namespace log4javascript
+{
+    /// <summary>
+    /// Logs messages to a pop-up console window (note: you will need to disable pop-up blockers to use it).
+    /// </summary>
+    public class PopUpAppender : Appender
+    {
+        public override JsString toString() { return default(JsString); }
+        public override void append(LoggingEvent loggingEvent) { }
+    }
+}
