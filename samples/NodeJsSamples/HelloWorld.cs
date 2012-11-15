12e721f78ee23fd6a4418e31132a8a668f79fbd9
lee.khn <lee.khn@gmail.com>
2012-11-15 08:32:31 +0000

    Add new project: NodeJs samples.

---
+++ samples/NodeJsSamples/HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SharpKit.Html;
+using SharpKit.NodeJs;
+
+namespace NodeJsSamples
+{
+    public class HelloWorld
+    {
+      
+    }
+}
