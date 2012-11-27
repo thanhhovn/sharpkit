0ad07bcd0bdeb541c20ed93220e27159e55b7d54
lee.khn@gmail.com <lee.khn@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2012-11-27 16:00:24 +0000

    Update SharpKit.NodeJs project.

--- trunk/SharpKit.NodeJs/net/Server.cs
+++ trunk/SharpKit.NodeJs/net/Server.cs
@@ -34,7 +34,7 @@ namespace SharpKit.NodeJs.net
         /// <p>Don&#39;t call <code>server.address()</code> until the <code>&#39;listening&#39;</code> event has been emitted.
         /// </p>
         /// </summary>
-        public object address(){return null;}
+        public object address() { return null; }
         /// <summary>
         /// <p>Stops the server from accepting new connections and keeps existing
         /// connections. This function is asynchronous, the server is finally
@@ -43,7 +43,7 @@ namespace SharpKit.NodeJs.net
         /// event.
         /// </p>
         /// </summary>
-        public object close(){return null;}
+        public object close() { return null; }
         /// <summary>
         /// <p>Stops the server from accepting new connections and keeps existing
         /// connections. This function is asynchronous, the server is finally
@@ -52,7 +52,7 @@ namespace SharpKit.NodeJs.net
         /// event.
         /// </p>
         /// </summary>
-        public object close(object callback){return null;}
+        public object close(JsAction callback) { return null; }
         /// <summary>
         /// <p>Begin accepting connections on the specified <code>port</code> and <code>host</code>.  If the
         /// <code>host</code> is omitted, the server will accept connections directed to any
@@ -83,7 +83,70 @@ namespace SharpKit.NodeJs.net
         /// <p>(Note: All sockets in Node set <code>SO_REUSEADDR</code> already)
         /// </p>
         /// </summary>
-        public object listen(object port){return null;}
+        public object listen(object port) { return null; }
+
+        /// <summary>
+        /// <p>Begin accepting connections on the specified <code>port</code> and <code>host</code>.  If the
+        /// <code>host</code> is omitted, the server will accept connections directed to any
+        /// IPv4 address (<code>INADDR_ANY</code>). A port value of zero will assign a random port.
+        /// </p>
+        /// <p>Backlog is the maximum length of the queue of pending connections.
+        /// The actual length will be determined by your OS through sysctl settings such as
+        /// <code>tcp_max_syn_backlog</code> and <code>somaxconn</code> on linux. The default value of this
+        /// parameter is 511 (not 512).
+        /// </p>
+        /// <p>This function is asynchronous.  When the server has been bound,
+        /// [&#39;listening&#39;][] event will be emitted.  The last parameter <code>callback</code>
+        /// will be added as an listener for the [&#39;listening&#39;][] event.
+        /// </p>
+        /// <p>One issue some users run into is getting <code>EADDRINUSE</code> errors. This means that
+        /// another server is already running on the requested port. One way of handling this
+        /// would be to wait a second and then try again. This can be done with
+        /// </p>
+        /// <pre><code>server.on(&#39;error&#39;, function (e) {
+        /// if (e.code == &#39;EADDRINUSE&#39;) {
+        /// console.log(&#39;Address in use, retrying...&#39;);
+        /// setTimeout(function () {
+        /// server.close();
+        /// server.listen(PORT, HOST);
+        /// }, 1000);
+        /// }
+        /// });</code></pre>
+        /// <p>(Note: All sockets in Node set <code>SO_REUSEADDR</code> already)
+        /// </p>
+        /// </summary>
+        public object listen(object port, object backlog, JsAction callback) { return null; }
+        /// <summary>
+        /// <p>Begin accepting connections on the specified <code>port</code> and <code>host</code>.  If the
+        /// <code>host</code> is omitted, the server will accept connections directed to any
+        /// IPv4 address (<code>INADDR_ANY</code>). A port value of zero will assign a random port.
+        /// </p>
+        /// <p>Backlog is the maximum length of the queue of pending connections.
+        /// The actual length will be determined by your OS through sysctl settings such as
+        /// <code>tcp_max_syn_backlog</code> and <code>somaxconn</code> on linux. The default value of this
+        /// parameter is 511 (not 512).
+        /// </p>
+        /// <p>This function is asynchronous.  When the server has been bound,
+        /// [&#39;listening&#39;][] event will be emitted.  The last parameter <code>callback</code>
+        /// will be added as an listener for the [&#39;listening&#39;][] event.
+        /// </p>
+        /// <p>One issue some users run into is getting <code>EADDRINUSE</code> errors. This means that
+        /// another server is already running on the requested port. One way of handling this
+        /// would be to wait a second and then try again. This can be done with
+        /// </p>
+        /// <pre><code>server.on(&#39;error&#39;, function (e) {
+        /// if (e.code == &#39;EADDRINUSE&#39;) {
+        /// console.log(&#39;Address in use, retrying...&#39;);
+        /// setTimeout(function () {
+        /// server.close();
+        /// server.listen(PORT, HOST);
+        /// }, 1000);
+        /// }
+        /// });</code></pre>
+        /// <p>(Note: All sockets in Node set <code>SO_REUSEADDR</code> already)
+        /// </p>
+        /// </summary>
+        public object listen(object port, object host, object backlog, JsAction callback) { return null; }
         /// <summary>
         /// <p>Start a UNIX socket server listening for connections on the given <code>path</code>.
         /// </p>
@@ -92,68 +155,29 @@ namespace SharpKit.NodeJs.net
         /// will be added as an listener for the [&#39;listening&#39;][] event.
         /// </p>
         /// </summary>
-        public object listen(object path, object callback){return null;}
+        public object listen(object path, JsAction callback) { return null; }
+       
+        //TODO: server.maxConnections, server.connections
+
+        #region Events
         /// <summary>
-        /// <p>Begin accepting connections on the specified <code>port</code> and <code>host</code>.  If the
-        /// <code>host</code> is omitted, the server will accept connections directed to any
-        /// IPv4 address (<code>INADDR_ANY</code>). A port value of zero will assign a random port.
-        /// </p>
-        /// <p>Backlog is the maximum length of the queue of pending connections.
-        /// The actual length will be determined by your OS through sysctl settings such as
-        /// <code>tcp_max_syn_backlog</code> and <code>somaxconn</code> on linux. The default value of this
-        /// parameter is 511 (not 512).
-        /// </p>
-        /// <p>This function is asynchronous.  When the server has been bound,
-        /// [&#39;listening&#39;][] event will be emitted.  The last parameter <code>callback</code>
-        /// will be added as an listener for the [&#39;listening&#39;][] event.
-        /// </p>
-        /// <p>One issue some users run into is getting <code>EADDRINUSE</code> errors. This means that
-        /// another server is already running on the requested port. One way of handling this
-        /// would be to wait a second and then try again. This can be done with
-        /// </p>
-        /// <pre><code>server.on(&#39;error&#39;, function (e) {
-        /// if (e.code == &#39;EADDRINUSE&#39;) {
-        /// console.log(&#39;Address in use, retrying...&#39;);
-        /// setTimeout(function () {
-        /// server.close();
-        /// server.listen(PORT, HOST);
-        /// }, 1000);
-        /// }
-        /// });</code></pre>
-        /// <p>(Note: All sockets in Node set <code>SO_REUSEADDR</code> already)
-        /// </p>
+        /// Emitted when the server has been bound after calling server.listen.
         /// </summary>
-        public object listen(object port, object backlog, object callback){return null;}
+        public JsAction listening { get; set; }
         /// <summary>
-        /// <p>Begin accepting connections on the specified <code>port</code> and <code>host</code>.  If the
-        /// <code>host</code> is omitted, the server will accept connections directed to any
-        /// IPv4 address (<code>INADDR_ANY</code>). A port value of zero will assign a random port.
-        /// </p>
-        /// <p>Backlog is the maximum length of the queue of pending connections.
-        /// The actual length will be determined by your OS through sysctl settings such as
-        /// <code>tcp_max_syn_backlog</code> and <code>somaxconn</code> on linux. The default value of this
-        /// parameter is 511 (not 512).
-        /// </p>
-        /// <p>This function is asynchronous.  When the server has been bound,
-        /// [&#39;listening&#39;][] event will be emitted.  The last parameter <code>callback</code>
-        /// will be added as an listener for the [&#39;listening&#39;][] event.
-        /// </p>
-        /// <p>One issue some users run into is getting <code>EADDRINUSE</code> errors. This means that
-        /// another server is already running on the requested port. One way of handling this
-        /// would be to wait a second and then try again. This can be done with
-        /// </p>
-        /// <pre><code>server.on(&#39;error&#39;, function (e) {
-        /// if (e.code == &#39;EADDRINUSE&#39;) {
-        /// console.log(&#39;Address in use, retrying...&#39;);
-        /// setTimeout(function () {
-        /// server.close();
-        /// server.listen(PORT, HOST);
-        /// }, 1000);
-        /// }
-        /// });</code></pre>
-        /// <p>(Note: All sockets in Node set <code>SO_REUSEADDR</code> already)
-        /// </p>
+        /// Emitted when a new connection is made. socket is an instance of net.Socket.
         /// </summary>
-        public object listen(object port, object host, object backlog, object callback){return null;}
+        public JsAction<Socket> connection { get; set; }
+        /// <summary>
+        /// Emitted when the server closes. Note that if connections exist, this event is not emitted until all connections are ended.
+        /// </summary>
+        [JsProperty(Name = "close")]
+        public JsAction closeEvent { get; set; }
+        /// <summary>
+        /// Emitted when an error occurs. The 'close' event will be called directly following this event. See example in discussion of server.listen.
+        /// </summary>
+        public JsAction<Object> error { get; set; }
+
+        #endregion
     }
 }
