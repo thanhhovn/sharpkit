00ab16c7bc44cef8beb7048b2e7a9f4a0dddf586
yvan.rodrigues@gmail.com <yvan.rodrigues@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2012-12-05 20:28:18 +0000

    Implemented System.Enum.Parse

--- trunk/SharpKit.JsClr/System/Enum.cs
+++ trunk/SharpKit.JsClr/System/Enum.cs
@@ -1,35 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-
-using System.Text;
-
 
 namespace SharpKit.JavaScript.Private
 {
-
 	[JsType(Name = "System.Enum", Filename = "~/Internal/Core.js")]
 	internal class JsImplEnum
 	{
-		public static string[] GetNames(Type type)
+        /// <summary>
+        /// Retrieves an array of the names of the constants in a specified enumeration.
+        /// </summary>
+        /// <param name="enumType">Type of the enum.</param>
+        /// <returns>A string array of the names of the constants in enumType.</returns>
+        /// <exception cref="ArgumentNullException"><para>enumType</para> is null.</exception>
+        public static string[] GetNames(Type enumType)
 		{
-			var jsType = type.As<JsImplType>()._JsType;
+            if (enumType == null) throw new ArgumentNullException("enumType");
+
+            var jsType = enumType.As<JsImplType>()._JsType;
 			var array = new JsExtendedArray();
 			foreach (var p in jsType.staticDefinition)
-			{
 				array.push(p);
-			}
 			return array.As<string[]>();
 		}
-		public static object[] GetValues(Type type)
+
+        /// <summary>
+        /// Retrieves an array of the values of the constants in a specified enumeration.
+        /// </summary>
+        /// <param name="enumType">Type of the enum.</param>
+        /// <returns>An array that contains the values of the constants in enumType.</returns>
+        /// <exception cref="ArgumentNullException"><para>enumType</para> is null.</exception>
+        public static object[] GetValues(Type enumType)
 		{
-			var jsType = type.As<JsImplType>()._JsType;
+            if (enumType == null) throw new ArgumentNullException("enumType");
+
+            var jsType = enumType.As<JsImplType>()._JsType;
 			var array = new JsExtendedArray();
 			foreach (var p in jsType.staticDefinition)
-			{
 				array.push(jsType.staticDefinition[p]);
-			}
 			return array.As<object[]>();
 		}
-	}
 
+        /// <summary>
+        /// Converts the string representation of the name or numeric value of one or more enumerated constants to an equivalent enumerated object.
+        /// </summary>
+        /// <param name="enumType">Type of the enum.</param>
+        /// <param name="value">A string containing the name or value to convert.</param>
+        /// <returns>An object of type enumType whose value is represented by value.</returns>
+        /// <exception cref="ArgumentNullException"><para>enumType</para> or <para>value</para> is null.</exception>
+        /// <exception cref="ArgumentException"><para>enumType</para> or <para>value</para> is null.</exception>
+        public static object Parse(Type enumType, String value)
+        {
+            if (enumType == null) throw new ArgumentNullException("enumType");
+            if (value == null) throw new ArgumentNullException("value");
+            if (value.Trim() == string.Empty) throw new ArgumentException("value is either an empty string or only contains white space.");
+
+            var jsType = enumType.As<JsImplType>()._JsType;
+            foreach (var p in jsType.staticDefinition)
+                if (p == value)
+                    return jsType.staticDefinition[p];
+            throw new ArgumentException("value is a name, but not one of the named constants defined for the enumeration.");
+        }
+	}
 }
