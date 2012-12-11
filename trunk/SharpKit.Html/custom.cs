59543d0c0cedeb5e79a59029899dbc2045b0e787
yvan.rodrigues@gmail.com <yvan.rodrigues@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2012-12-11 18:27:39 +0000

    Implemented indexer in SharpKit.Html.storage.Storage

--- trunk/SharpKit.Html/custom.cs
+++ trunk/SharpKit.Html/custom.cs
@@ -323,6 +323,7 @@ namespace SharpKit.Html.storage
 {
     partial class Storage
     {
+        [JsProperty(NativeIndexer = true)]
         public string this[string key]
         {
             get { return getItem(key); }
