b50b3c7c407b5cde490e0dd620f7a7e90fc5490e
lee.khn <lee.khn@gmail.com>
2012-12-20 09:17:43 +0000

    Update NodeJs project.

---
+++ SharpKit.NodeJs/console.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpKit.JavaScript;
+
+namespace SharpKit.NodeJs
+{
+    public static partial class console
+    {
+        public static void log(params object[] obj) { }
+        public static void log(JsString data) { }
+    }
+}
