a55d85d88ccc2f2fde87fcd9eb5a00429ad6e7ea
lee.khn <lee.khn@gmail.com>
2012-12-30 09:04:38 +0000

    Update SharpKit.NodeJs

--- SharpKit.NodeJs/dns/utils.cs
+++ SharpKit.NodeJs/dns/utils.cs
@@ -7,7 +7,7 @@ using SharpKit.JavaScript;
 namespace SharpKit.NodeJs.dns
 {
     [JsType(JsMode.Prototype, Export = false, Name = "dns")]
-    class utils
+    public partial class utils
     {
         /// <summary>
         /// Resolves a domain (e.g. 'google.com') into the first found A (IPv4) or AAAA (IPv6) record. 
@@ -19,7 +19,7 @@ namespace SharpKit.NodeJs.dns
         /// The address argument is a string representation of a IP v4 or v6 address. T
         /// he family argument is either the integer 4 or 6 and denotes the family of address (not necessarily the value initially passed to lookup).</param>
         /// <returns></returns>
-        public object lookup(JsString domain, JsAction<JsError, JsString, JsNumber> callback) { return null; }
+        public object lookup(JsString domain, JsAction<JsNumber, JsString, JsNumber> callback) { return null; }
         /// <summary>
         /// Resolves a domain (e.g. 'google.com') into the first found A (IPv4) or AAAA (IPv6) record. 
         /// On error, err is an Error object, where err.code is the error code. 
@@ -31,7 +31,7 @@ namespace SharpKit.NodeJs.dns
         /// The address argument is a string representation of a IP v4 or v6 address. T
         /// he family argument is either the integer 4 or 6 and denotes the family of address (not necessarily the value initially passed to lookup).</param>
         /// <returns></returns>
-        public object lookup(JsString domain, object family, JsAction<JsError, JsString, JsNumber> callback) { return null; }
+        public object lookup(JsString domain, object family, JsAction<JsNumber, JsString, JsNumber> callback) { return null; }
         /// <summary>
         /// Resolves a domain (e.g. 'google.com') into an array of the record types specified by rrtype.
         /// On error, err is an Error object, where err.code is one of the error codes listed below.
@@ -42,7 +42,7 @@ namespace SharpKit.NodeJs.dns
         /// and described in the documentation for the corresponding lookup methods below.
         /// </param>
         /// <returns></returns>
-        public object resolve(JsString domain, JsAction<JsError, JsString> callback) { return null; }
+        public object resolve(JsString domain, JsAction<JsNumber, JsString> callback) { return null; }
         /// <summary>
         /// Resolves a domain (e.g. 'google.com') into an array of the record types specified by rrtype.
         /// On error, err is an Error object, where err.code is one of the error codes listed below.
@@ -57,7 +57,7 @@ namespace SharpKit.NodeJs.dns
         /// and described in the documentation for the corresponding lookup methods below.
         /// </param>
         /// <returns></returns>
-        public object resolve(JsString domain, JsString rrtype, JsAction<JsError, object> callback) { return null; }
+        public object resolve(JsString domain, JsString rrtype, JsAction<JsNumber, object> callback) { return null; }
         /// <summary>
         /// The same as dns.resolve(), but only for IPv4 queries (A records). 
         /// addresses is an array of IPv4 addresses (e.g. ['74.125.79.104', '74.125.79.105', '74.125.79.106']).
@@ -65,14 +65,14 @@ namespace SharpKit.NodeJs.dns
         /// <param name="domain"></param>
         /// <param name="callback"></param>
         /// <returns></returns>
-        public object resolve4(JsString domain, JsAction<JsError, JsArray<JsString>> callback) { return null; }
+        public object resolve4(JsString domain, JsAction<JsNumber, JsArray<JsString>> callback) { return null; }
         /// <summary>
         /// The same as dns.resolve4() except for IPv6 queries (an AAAA query).
         /// </summary>
         /// <param name="domain"></param>
         /// <param name="callback"></param>
         /// <returns></returns>
-        public object resolve6(JsString domain, JsAction<JsError, JsArray<JsString>> callback) { return null; }
+        public object resolve6(JsString domain, JsAction<JsNumber, JsArray<JsString>> callback) { return null; }
         /// <summary>
         /// The same as dns.resolve(), but only for mail exchange queries (MX records).
         /// </summary>
@@ -81,14 +81,14 @@ namespace SharpKit.NodeJs.dns
         /// addresses is an array of MX records, each with a priority and an exchange attribute (e.g. [{'priority': 10, 'exchange': 'mx.example.com'},...]).
         /// </param>
         /// <returns></returns>
-        public object resolveMx(JsString domain, JsAction<JsError, JsArray<MxRecordsOptions>> callback) { return null; }
+        public object resolveMx(JsString domain, JsAction<JsNumber, JsArray<MxRecordsOptions>> callback) { return null; }
         /// <summary>
         /// The same as dns.resolve(), but only for text queries (TXT records). addresses is an array of the text records available for domain (e.g., ['v=spf1 ip4:0.0.0.0 ~all']).
         /// </summary>
         /// <param name="domain"></param>
         /// <param name="callback"></param>
         /// <returns></returns>
-        public object resolveTxt(JsString domain, JsAction<JsError, JsString> callback) { return null; }
+        public object resolveTxt(JsString domain, JsAction<JsNumber, JsString> callback) { return null; }
         /// <summary>
         /// The same as dns.resolve(), but only for service records (SRV records).
         /// </summary>
@@ -98,7 +98,7 @@ namespace SharpKit.NodeJs.dns
         /// Properties of SRV records are priority, weight, port, and name (e.g., [{'priority': 10, {'weight': 5, 'port': 21223, 'name': 'service.example.com'}, ...]).
         /// </param>
         /// <returns></returns>
-        public object resolveSrv(JsString domain, JsAction<JsError, SrvRecordsOptions> callback) { return null; }
+        public object resolveSrv(JsString domain, JsAction<JsNumber, SrvRecordsOptions> callback) { return null; }
         /// <summary>
         /// The same as dns.resolve(), but only for name server records (NS records). 
         /// </summary>
@@ -108,7 +108,7 @@ namespace SharpKit.NodeJs.dns
         /// (e.g., ['ns1.example.com', 'ns2.example.com']).
         /// </param>
         /// <returns></returns>
-        public object resolveNs(JsString domain, JsAction<JsError, JsArray<JsString>> callback) { return null; }
+        public object resolveNs(JsString domain, JsAction<JsNumber, JsArray<JsString>> callback) { return null; }
         /// <summary>
         /// The same as dns.resolve(), but only for canonical name records (CNAME records). 
         /// </summary>
@@ -117,7 +117,7 @@ namespace SharpKit.NodeJs.dns
         /// addresses is an array of the canonical name records available for domain (e.g., ['bar.example.com']).
         /// </param>
         /// <returns></returns>
-        public object resolveCname(JsString domain, JsAction<JsError, JsArray<JsString>> callback) { return null; }
+        public object resolveCname(JsString domain, JsAction<JsNumber, JsArray<JsString>> callback) { return null; }
         /// <summary>
         /// Reverse resolves an ip address to an array of domain names.
         /// On error, err is an Error object, where err.code is one of the error codes listed below.
@@ -125,7 +125,7 @@ namespace SharpKit.NodeJs.dns
         /// <param name="ip"></param>
         /// <param name="callback">The callback has arguments (err, domains).</param>
         /// <returns></returns>
-        public object reverse(JsString ip, JsAction<JsError, JsArray<JsString>> callback) { return null; }
+        public object reverse(JsString ip, JsAction<JsNumber, JsArray<JsString>> callback) { return null; }
     }
     [JsType(JsMode.Json)]
     public partial class SrvRecordsOptions
@@ -142,32 +142,107 @@ namespace SharpKit.NodeJs.dns
         public JsNumber priority { get; set; }
         public JsString exchange { get; set; }
     }
-
-    //TODO: Error codes
-    //    Each DNS query can return one of the following error codes:
-
-    //dns.NODATA: DNS server returned answer with no data.
-    //dns.FORMERR: DNS server claims query was misformatted.
-    //dns.SERVFAIL: DNS server returned general failure.
-    //dns.NOTFOUND: Domain name not found.
-    //dns.NOTIMP: DNS server does not implement requested operation.
-    //dns.REFUSED: DNS server refused query.
-    //dns.BADQUERY: Misformatted DNS query.
-    //dns.BADNAME: Misformatted domain name.
-    //dns.BADFAMILY: Unsupported address family.
-    //dns.BADRESP: Misformatted DNS reply.
-    //dns.CONNREFUSED: Could not contact DNS servers.
-    //dns.TIMEOUT: Timeout while contacting DNS servers.
-    //dns.EOF: End of file.
-    //dns.FILE: Error reading file.
-    //dns.NOMEM: Out of memory.
-    //dns.DESTRUCTION: Channel is being destroyed.
-    //dns.BADSTR: Misformatted string.
-    //dns.BADFLAGS: Illegal flags specified.
-    //dns.NONAME: Given hostname is not numeric.
-    //dns.BADHINTS: Illegal hints flags specified.
-    //dns.NOTINITIALIZED: c-ares library initialization not yet performed.
-    //dns.LOADIPHLPAPI: Error loading iphlpapi.dll.
-    //dns.ADDRGETNETWORKPARAMS: Could not find GetNetworkParams function.
-    //dns.CANCELLED: DNS query cancelled.
+    /// <summary>
+    ///     Each DNS query can return one of the following error codes:
+    /// </summary>
+    //TODO: json mode?
+    public partial class dns
+    {
+        /// <summary>
+        /// DNS server returned answer with no data.
+        /// </summary>
+        public static int NODATA { get; set; }
+        /// <summary>
+        /// DNS server claims query was misformatted.
+        /// </summary>
+        public static int FORMERR { get; set; }
+        /// <summary>
+        ///  DNS server returned general failure.
+        /// </summary>
+        public static int SERVFAIL { get; set; }
+        /// <summary>
+        /// Domain name not found.
+        /// </summary>
+        public static int NOTFOUND { get; set; }
+        /// <summary>
+        /// DNS server does not implement requested operation.
+        /// </summary>
+        public static int NOTIMP { get; set; }
+        /// <summary>
+        /// DNS server refused query.
+        /// </summary>
+        public static int REFUSED { get; set; }
+        /// <summary>
+        /// Misformatted DNS query.
+        /// </summary>
+        public static int BADQUERY { get; set; }
+        /// <summary>
+        /// Misformatted domain name.
+        /// </summary>
+        public static int BADNAME { get; set; }
+        /// <summary>
+        /// Unsupported address family.
+        /// </summary>
+        public static int BADFAMILY { get; set; }
+        /// <summary>
+        /// Misformatted DNS reply.
+        /// </summary>
+        public static int BADRESP { get; set; }
+        /// <summary>
+        /// Could not contact DNS servers.
+        /// </summary>
+        public static int CONNREFUSED { get; set; }
+        /// <summary>
+        /// Timeout while contacting DNS servers.
+        /// </summary>
+        public static int TIMEOUT { get; set; }
+        /// <summary>
+        /// End of file.
+        /// </summary>
+        public static int EOF { get; set; }
+        /// <summary>
+        /// Error reading file.
+        /// </summary>
+        public static int FILE { get; set; }
+        /// <summary>
+        /// Out of memory.
+        /// </summary>
+        public static int NOMEM { get; set; }
+        /// <summary>
+        /// Channel is being destroyed.
+        /// </summary>
+        public static int DESTRUCTION { get; set; }
+        /// <summary>
+        /// Misformatted string.
+        /// </summary>
+        public static int BADSTR { get; set; }
+        /// <summary>
+        /// Illegal flags specified.
+        /// </summary>
+        public static int BADFLAGS { get; set; }
+        /// <summary>
+        /// Given hostname is not numeric.
+        /// </summary>
+        public static int NONAME { get; set; }
+        /// <summary>
+        /// Illegal hints flags specified.
+        /// </summary>
+        public static int BADHINTS { get; set; }
+        /// <summary>
+        /// c-ares library initialization not yet performed.
+        /// </summary>
+        public static int NOTINITIALIZED { get; set; }
+        /// <summary>
+        /// Error loading iphlpapi.dll.
+        /// </summary>
+        public static int LOADIPHLPAPI { get; set; }
+        /// <summary>
+        /// Could not find GetNetworkParams function.
+        /// </summary>
+        public static int ADDRGETNETWORKPARAMS { get; set; }
+        /// <summary>
+        /// DNS query cancelled.
+        /// </summary>
+        public static int CANCELLED { get; set; }
+    }
 }
