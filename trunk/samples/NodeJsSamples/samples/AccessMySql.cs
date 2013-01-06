a3198cdbdb83c43896dbf334845dd643d6409d5d
lee.khn@gmail.com <lee.khn@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2013-01-06 15:39:39 +0000

    Add new NodeJs samples.

---
+++ trunk/samples/NodeJsSamples/samples/AccessMySql.cs
@@ -0,0 +1,50 @@
+﻿using SharpKit.JavaScript;
+using SharpKit.NodeJs.events;
+using SharpKit.Html;
+using SharpKit.NodeJs.http;
+using SharpKit.NodeJs;
+namespace NodeJsSamples.samples
+{
+    [JsType(JsMode.Global)]
+    public class AccessMySql
+    {
+        static void OnReady()
+        {
+            // Include http module,
+            var http = require("http").As<SharpKit.NodeJs.http.Utils>();
+            // And mysql module you've just installed.
+            var mysql = require("mysql").As<SharpKit.NodeJs.net.Utils>();
+            // Create the connection.
+            // Data is default to new mysql installation and should be changed according to your configuration.
+            var connection = mysql.createConnection(new
+            {
+                user = "root",
+                password = "",
+                database = "db_name"
+            }).As<AccessMySql>();
+            // Create the http server.
+            http.createServer((request, response) =>
+            {
+                // Attach listener on end event.
+                request.As<EventEmitter>().on("end", () =>
+                {
+                    // Query the database.
+                    Connection.query("SELECT * FROM your_table;", (error, rows, fields) =>
+                    {
+                        response.writeHead(200, new
+                        {
+                            ContentType = "x-application/json"
+                        });
+                        // Send data as JSON string.
+                        // Rows variable holds the result of the query.
+                        response.end(JSON.stringify(rows));
+                    });
+                });
+                // Listen on the 8080 port.
+            }).As<Server>().listen(8080);
+
+        }
+        static object require(JsString className) { return null; }
+       
+    }
+}
---
+++ trunk/samples/NodeJsSamples/samples/UrlParam.cs
@@ -0,0 +1,39 @@
+﻿using SharpKit.JavaScript;
+using SharpKit.NodeJs.events;
+using SharpKit.NodeJs;
+using SharpKit.NodeJs.http;
+namespace NodeJsSamples.samples
+{
+    [JsType(JsMode.Global)]
+    public class UrlParam
+    {
+        static void OnReady()
+        {
+            // Include http module,
+            var http = require("http");
+            // And url module, which is very helpful in parsing request parameters.
+            var url = require("url").As<Url>();
+            // Create the server.
+            http.createServer((request, response) =>
+            {
+                // Attach listener on end event.
+                request.As<EventEmitter>().on("end", () =>
+                {
+                    // Parse the request for arguments and store them in _get variable.
+                    // This function parses the url from request and returns object representation.
+                    var _get = Url.parse(request.As<Request>().url, true).query;
+                    // Write headers to the response.
+                    response.writeHead(200, new
+                    {
+                        ContentType = "text/plain"
+                    });
+                    // Send data and end response.
+                    response.end("Here is your data: " + _get.As<JsObject>()["data"]);
+                });
+                // Listen on the 8080 port.
+            }).As<Server>().listen(8080);
+
+        }
+        static SharpKit.NodeJs.http.Utils require(JsString className) { return null; }
+    }
+}
