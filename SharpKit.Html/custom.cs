b390fa28fe18b9fe62b185ef2381810531a83d2f
yvan.rodrigues <yvan.rodrigues@gmail.com>
2013-01-09 21:14:04 +0000

    Window now implements ILocalFileSystem.

--- SharpKit.Html/custom.cs
+++ SharpKit.Html/custom.cs
@@ -319,15 +319,3 @@ namespace SharpKit.Html
     //
 }
 
-namespace SharpKit.Html.storage
-{
-    partial class Storage
-    {
-        [JsProperty(NativeIndexer = true)]
-        public string this[string key]
-        {
-            get { return getItem(key); }
-            set { setItem(key, value); }
-        }
-    }
-}
