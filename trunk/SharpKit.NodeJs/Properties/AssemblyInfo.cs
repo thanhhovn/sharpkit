396fd75aeda03b1ab721be5d2e4b9057e4c0b220
DanelKhen@gmail.com <DanelKhen@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2013-01-22 11:29:59 +0000

    Added missing SharpKit attributes in NodeJs library (credit: Damien)

--- trunk/SharpKit.NodeJs/Properties/AssemblyInfo.cs
+++ trunk/SharpKit.NodeJs/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using SharpKit.JavaScript;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("4.30.2000")]
+[assembly:JsType(JsMode.Prototype, Export=false)]
+[assembly: JsNamespace(Namespace="SharpKit.NodeJs", JsNamespace="")]
