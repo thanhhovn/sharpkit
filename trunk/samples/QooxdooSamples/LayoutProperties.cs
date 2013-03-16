678ca35b6e18708657bd87eaa30d3514993a8b34
nik.suslin@gmail.com <nik.suslin@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2013-03-16 16:08:23 +0000

    Added sample project for the Qooxdoo wrapper

---
+++ trunk/samples/QooxdooSamples/LayoutProperties.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SharpKit.JavaScript;
+
+namespace QooxDooTest
+{
+    [JsType(JsMode.Json)]
+    public class LayoutProperties
+    {
+        [JsProperty(Name="left")]
+        public object Left { get; set; }
+        [JsProperty(Name = "top")]
+        public object Top { get; set; }
+        [JsProperty(Name = "right")]
+        public object Right { get; set; }
+        [JsProperty(Name = "bottom")]
+        public object Bottom { get; set; }
+        [JsProperty(Name = "width")]
+        public JsString Width { get; set; }
+        [JsProperty(Name = "height")]
+        public JsString Height { get; set; }
+    }
+}
