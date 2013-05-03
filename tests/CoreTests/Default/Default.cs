a2520d2af6819a3cb34528920abd37cdee45b2ae
danelkhen <DanelKhen@gmail.com>
2013-05-03 16:04:23 +0000

    Add some more enum unit tests

---
+++ tests/CoreTests/Default/Default.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpKit.JavaScript;
+
+namespace CoreTests.Default
+{
+    [JsType(JsMode.Clr)]
+    class Default
+    {
+        void foo()
+        {
+            var y = default(MyEnum);
+        }
+    }
+
+    [JsType(JsMode.Clr)]
+    enum MyEnum
+    {
+        A,
+        B,
+        C
+    }
+}
