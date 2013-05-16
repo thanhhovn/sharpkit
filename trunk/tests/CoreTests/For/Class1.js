474c39d8fce9e1c4860505e2c66c7f955e423ae9
DanelKhen@gmail.com <DanelKhen@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2013-05-16 19:32:33 +0000

    Add for loops unit tests

---
+++ trunk/tests/CoreTests/For/Class1.js
@@ -0,0 +1,26 @@
+if (typeof(CoreTests) == "undefined")
+    var CoreTests = {};
+if (typeof(CoreTests.For) == "undefined")
+    CoreTests.For = {};
+CoreTests.For.HasNext = function ()
+{
+    this.Next = null;
+};
+CoreTests.For.Bug249 = function ()
+{
+};
+CoreTests.For.Bug249.Foo = function ()
+{
+    var p = new CoreTests.For.HasNext();
+    var count = 0;
+    for (; p != null; p = p.Next, count++)
+    {
+    }
+    var x = 0, y = 0;
+    for (x = 7, y = 8; x < 7 && y < 8; x++, y++)
+    {
+    }
+    for (var a = 7, b = 8; x < 7 && y < 8; x++, y++)
+    {
+    }
+};
