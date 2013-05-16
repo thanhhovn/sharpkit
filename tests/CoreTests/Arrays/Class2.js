b69ad295a92f98b26c0b345f9cf08504ab0985c3
danelkhen <DanelKhen@gmail.com>
2013-05-16 16:04:32 +0000

    Add tests for typed arrays

--- tests/CoreTests/Arrays/Class2.js
+++ tests/CoreTests/Arrays/Class2.js
@@ -1,20 +1,40 @@
-if (typeof(JsTypes) == "undefined")
-    var JsTypes = [];
-var SharpKitWebApp5$Class2 =
+if (typeof(Int8Array) == "undefined")
+    var Int8Array = Array;
+if (typeof(Int16Array) == "undefined")
+    var Int16Array = Array;
+if (typeof(Uint16Array) == "undefined")
+    var Uint16Array = Array;
+if (typeof(Int32Array) == "undefined")
+    var Int32Array = Array;
+if (typeof(Uint32Array) == "undefined")
+    var Uint32Array = Array;
+if (typeof(Float32Array) == "undefined")
+    var Float32Array = Array;
+if (typeof(Float64Array) == "undefined")
+    var Float64Array = Array;
+if (typeof(SharpKitWebApp5) == "undefined")
+    var SharpKitWebApp5 = {};
+SharpKitWebApp5.Class2 = function ()
 {
-    fullname: "SharpKitWebApp5.Class2",
-    baseTypeName: "System.Object",
-    assemblyName: "CoreTests",
-    Kind: "Class",
-    definition:
-    {
-        ctor: function ()
-        {
-            System.Object.ctor.call(this);
-        },
-        foo: function ()
-        {
-        }
-    }
 };
-JsTypes.push(SharpKitWebApp5$Class2);
+SharpKitWebApp5.Class2.prototype.foo = function ()
+{
+    var list =  [];
+    list.push(new Int8Array(7));
+    list.push(new Int16Array(7));
+    list.push(new Uint16Array(7));
+    list.push(new Int32Array(7));
+    list.push(new Uint32Array(7));
+    list.push(new Float32Array(7));
+    list.push(new Float64Array(7));
+    list.push(new Int8Array(7, 8, 9));
+    list.push(new Int16Array(7, 8, 9));
+    list.push(new Uint16Array(7, 8, 9));
+    list.push(new Int32Array(7, 8, 9));
+    list.push(new Uint32Array(7, 8, 9));
+    list.push(new Float32Array(7, 8, 9));
+    list.push(new Float64Array(7, 8, 9));
+    list.push(new Int32Array(7, 8, 9));
+    list.push(new Float64Array(7.7, 8.8, 9.9));
+    list.push(new Float64Array(7, 8, 9));
+};
