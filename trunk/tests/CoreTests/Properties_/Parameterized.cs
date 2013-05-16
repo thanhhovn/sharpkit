bf29f76bf6ce348e845a9410608e0a1000d12725
sebastian.loncar@gmail.com <sebastian.loncar@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2013-05-16 23:28:43 +0000

    added parameterized constructor test

---
+++ trunk/tests/CoreTests/Properties_/Parameterized.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpKit.JavaScript;
+
+namespace CoreTests.Properties_
+{
+
+    [JsType(JsMode.Clr)]
+    internal static class ClrParameterizedTest
+    {
+
+        public static void CreateColorFromExpression()
+        {
+            var obj = new ClrClass() { Property = 5 };
+        }
+
+    }
+
+    [JsType(JsMode.Clr)]
+    public class ClrClass
+    {
+        public int Property { get; set; }
+    }
+
+}
