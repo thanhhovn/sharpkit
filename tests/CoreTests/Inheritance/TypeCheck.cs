465911444730dfa0d1c51e57fbbf2d9e820fdcf0
sebastian.loncar <sebastian.loncar@gmail.com>
2013-05-16 23:35:48 +0000

    added type check test

---
+++ tests/CoreTests/Inheritance/TypeCheck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpKit.JavaScript;
+using SharpKit.Html;
+
+namespace CoreTests.Inheritance
+{
+
+    [JsType]
+    class TypeCheck
+    {
+        public void test(object obj)
+        {
+            if (obj is HtmlElement)
+            {
+            }
+        }
+    }
+
+}
