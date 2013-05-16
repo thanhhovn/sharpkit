b08aeac5b9c410b1df2b1543717a2a0314c0b6a8
danelkhen <DanelKhen@gmail.com>
2013-05-16 19:32:33 +0000

    Add for loops unit tests

---
+++ tests/CoreTests/For/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpKit.JavaScript;
+
+namespace CoreTests.For
+{
+    [JsType(JsMode.Prototype)]
+    class HasNext
+    {
+        public HasNext Next { get; set; }
+    }
+    [JsType(JsMode.Prototype)]
+    class Bug249
+    {
+        static void Foo()
+        {
+            var p = new HasNext();
+            var count = 0;
+            for (; p != null; p = p.Next, count++)
+            {
+            }
+
+            int x = 0, y = 0;
+
+            for (x = 7, y = 8; x < 7 && y < 8; x++, y++)
+            {
+            }
+
+            for (int a = 7, b = 8; x < 7 && y < 8; x++, y++)
+            {
+            }
+        }
+    }
+}
+
