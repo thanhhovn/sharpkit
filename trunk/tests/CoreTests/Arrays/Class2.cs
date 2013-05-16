afdb8ae7d59fc840a2af9faf373a1028e8d844ee
DanelKhen@gmail.com <DanelKhen@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2013-05-16 16:04:32 +0000

    Add tests for typed arrays

--- trunk/tests/CoreTests/Arrays/Class2.cs
+++ trunk/tests/CoreTests/Arrays/Class2.cs
@@ -2,13 +2,33 @@
 
 namespace SharpKitWebApp5
 {
-    [JsType]
+    [JsType(JsMode.Prototype)]
     public class Class2
     {
         void foo()
         {
-            //var x = new int[7, 8];
-            //x[4, 5] = 9;
+            var list = new JsArray<object>();
+            list.push(new byte[7]);
+            list.push(new short[7]);
+            list.push(new ushort[7]);
+            list.push(new int[7]);
+            list.push(new uint[7]);
+            list.push(new float[7]);
+            list.push(new double[7]);
+
+
+            list.push(new byte[] { 7, 8, 9 });
+            list.push(new short[] { 7, 8, 9 });
+            list.push(new ushort[] { 7, 8, 9 });
+            list.push(new int[] { 7, 8, 9 });
+            list.push(new uint[] { 7, 8, 9 });
+            list.push(new float[] { 7, 8, 9 });
+            list.push(new double[] { 7, 8, 9 });
+
+
+            list.push(new[] { 7, 8, 9 });
+            list.push(new[] { 7.7, 8.8, 9.9 });
+            list.push(new[] { 7d, 8d, 9d });
         }
     }
 }
