4e05cf0f3c7fa34958a7a83577a9cae42ec7755b
DanelKhen@gmail.com <DanelKhen@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2013-07-03 07:41:12 +0000

    Create NoClr test project

---
+++ trunk/tests/NoClrTests/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpKit.JavaScript;
+
+namespace NoClrTests
+{
+    [JsType(JsMode.Prototype)]
+    class Test
+    {
+        List<int> list;
+        public void Test2()
+        {
+            var x = list.Count;
+        }
+    }
+}
