6a8c5665a8a425ab3784eb335bc52399a1fbfc05
DanelKhen@gmail.com <DanelKhen@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2013-07-04 07:05:22 +0000

    Add Operator overloading unit tests

---
+++ trunk/tests/CoreTests/OperatorOverloads/OperatorOverloads.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpKit.JavaScript;
+
+namespace CoreTests.OperatorOverloads
+{
+    [JsType(JsMode.Prototype)]
+    class Test
+    {
+        void foo()
+        {
+            var x = new BigNumber();
+            var y = new BigNumber();
+            x += y;
+            var z = x + y;
+            var a = MyBigNumber + x;
+            MyBigNumber += a;
+            
+        }
+        [JsProperty(NativeField=false)]
+        public BigNumber MyBigNumber { get; set; }
+    }
+    [JsType(JsMode.Prototype)]
+    class BigNumber
+    {
+        public static BigNumber operator +(BigNumber x, BigNumber y)
+        {
+            return new BigNumber();
+        }
+    }
+}
