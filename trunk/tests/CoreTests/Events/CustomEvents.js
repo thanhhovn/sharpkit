3b6268645e2e5da4012b52dadb0c110edcb00986
DanelKhen@gmail.com <DanelKhen@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2013-09-12 14:11:49 +0000

    Added extension methods and custom events tests

--- trunk/tests/CoreTests/Events/CustomEvents.js
+++ trunk/tests/CoreTests/Events/CustomEvents.js
@@ -44,6 +44,36 @@ if (typeof(CoreTests) == "undefined")
     var CoreTests = {};
 if (typeof(CoreTests.Events) == "undefined")
     CoreTests.Events = {};
+CoreTests.Events.CustomEvents = function ()
+{
+    this.MyEvent2 = null;
+    this.MyEvent = null;
+    this.MyEvent3 = null;
+};
+CoreTests.Events.CustomEvents.prototype.add_MyEvent2 = function (value)
+{
+}
+;
+CoreTests.Events.CustomEvents.prototype.remove_MyEvent2 = function (value)
+{
+}
+;
+CoreTests.Events.CustomEvents.prototype.MyEvent.add = function (value)
+{
+}
+;
+CoreTests.Events.CustomEvents.prototype.MyEvent.remove = function (value)
+{
+}
+;
+CoreTests.Events.CustomEvents.prototype.add_MyEvent3 = function (value)
+{
+}
+;
+CoreTests.Events.CustomEvents.prototype.remove_MyEvent3 = function (value)
+{
+}
+;
 CoreTests.Events.CustomEventsTest = function ()
 {
 };
@@ -52,6 +82,8 @@ CoreTests.Events.CustomEventsTest.prototype.foo = function ()
     var x = new CoreTests.Events.CustomEvents();
     x.MyEvent.add($CreateDelegate(this, this.x_MyEvent));
     x.MyEvent.remove($CreateDelegate(this, this.x_MyEvent));
+    x.add_MyEvent2($CreateDelegate(this, this.x_MyEvent));
+    x.remove_MyEvent2($CreateDelegate(this, this.x_MyEvent));
 };
 CoreTests.Events.CustomEventsTest.prototype.x_MyEvent = function ()
 {
