d83a9d21014a69be68e42954cf9050c8aa945b38
sebastian.loncar <sebastian.loncar@gmail.com>
2013-12-03 22:22:28 +0000

    added field tests

---
+++ tests/CoreTests/Fields/Fields.js
@@ -0,0 +1,17 @@
+if (typeof(CoreTests) == "undefined")
+    var CoreTests = {};
+if (typeof(CoreTests.For) == "undefined")
+    CoreTests.For = {};
+CoreTests.For.FieldsTest = function ()
+{
+    this.a2 = null;
+    this.a = "aaa";
+    this.b = "bbb";
+    this.c = "ccc";
+    this.a1 = 1;
+    this.b1 = 0;
+    this.c1 = 3;
+    this.a3 = "aa";
+    this.list = new System.Collections.ArrayList.ctor();
+    this.a4 = new System.Collections.Generic.List$1.ctor$$IEnumerable$1(System.String.ctor, ["a", "b", "c"]);
+};
