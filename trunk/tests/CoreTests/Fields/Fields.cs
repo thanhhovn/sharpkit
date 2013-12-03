15b27b05a5b970739ab1fa2a79ddc6050d7c60a5
sebastian.loncar@gmail.com <sebastian.loncar@gmail.com@8bcafed8-041a-11df-8c27-7d37615f5189>
2013-12-03 22:22:28 +0000

    added field tests

---
+++ trunk/tests/CoreTests/Fields/Fields.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpKit.JavaScript;
+
+namespace CoreTests.For
+{
+    [JsType(JsMode.Prototype)]
+    class FieldsTest
+    {
+        public string a2;
+        public string a = "aaa", b = "bbb", c = "ccc";
+        public int a1 = 1, b1, c1 = 3;
+        public string a3 = "aa";
+        public System.Collections.ArrayList list = new System.Collections.ArrayList();
+        public System.Collections.Generic.List<string> a4 = new System.Collections.Generic.List<string>(new string[] { "a", "b", "c" });
+
+    }
+}
+
