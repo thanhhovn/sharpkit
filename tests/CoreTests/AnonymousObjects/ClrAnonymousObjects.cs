0e300eafdf79acbf234c918d00d7453d076895a9
danelkhen <DanelKhen@gmail.com>
2013-12-14 12:51:46 +0000

    Added tests for anonymous objects, LinqQueries, Indexers

---
+++ tests/CoreTests/AnonymousObjects/ClrAnonymousObjects.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpKit.JavaScript;
+
+namespace CoreTests.AnonymousObjects
+{
+    [JsType(JsMode.Prototype, NativeJsons = false)]
+    public class NonNativeJsons
+    {
+        public void F()
+        {
+            var xx = new object();
+            var x = new { a = "b" };
+            var a = x.a;
+        }
+    }
+}
---
+++ tests/CoreTests/LinqQueries/LinqQueries.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpKit.JavaScript;
+
+namespace CoreTests.LinqQueries
+{
+    [JsType(JsMode.Prototype)]
+    class LinqQueries
+    {
+        void foo()
+        {
+            string[] list = new[] { "a", "b", "c" };
+            var q =
+                from sentence in list
+                let words = sentence.Split(' ')
+                from word in words
+                let w = word.ToLower()
+                where w.StartsWith("a")
+                select word;
+        }
+    }
+}
