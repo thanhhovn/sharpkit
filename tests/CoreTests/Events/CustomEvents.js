dbc94be8c0014118e96d679039d8e925ee0289bc
danelkhen <DanelKhen@gmail.com>
2014-01-11 10:25:34 +0000

    Fix autoamtic events bug (Credit: Daniel)

--- tests/CoreTests/Events/CustomEvents.js
+++ tests/CoreTests/Events/CustomEvents.js
@@ -12,6 +12,87 @@ if (typeof($CreateException)=='undefined')
         return error;
     }
 }
+function $CombineDelegates(del1,del2)
+{
+    if(del1 == null)
+        return del2;
+    if(del2 == null)
+        return del1;
+    var del=$CreateMulticastDelegateFunction();
+    del.delegates = [];
+    if(del1.isMulticastDelegate)
+    {
+        for(var i=0;i < del1.delegates.length;i++)
+            del.delegates.push(del1.delegates[i]);
+    }
+    else
+    {
+        del.delegates.push(del1);
+    }
+    if(del2.isMulticastDelegate)
+    {
+        for(var i=0;i < del2.delegates.length;i++)
+            del.delegates.push(del2.delegates[i]);
+    }
+    else
+    {
+        del.delegates.push(del2);
+    }
+    return del;
+};
+function $CreateMulticastDelegateFunction()
+{
+    var del2 = null;
+    
+    var del=function()
+    {
+        var x=undefined;
+        for(var i=0;i < del2.delegates.length;i++)
+        {
+            var del3=del2.delegates[i];
+            x = del3.apply(null,arguments);
+        }
+        return x;
+    };
+    del.isMulticastDelegate = true;
+    del2 = del;   
+    
+    return del;
+};
+function $RemoveDelegate(delOriginal,delToRemove)
+{
+    if(delToRemove == null || delOriginal == null)
+        return delOriginal;
+    if(delOriginal.isMulticastDelegate)
+    {
+        if(delToRemove.isMulticastDelegate)
+            throw new Error("Multicast to multicast delegate removal is not implemented yet");
+        var del=$CreateMulticastDelegateFunction();
+        for(var i=0;i < delOriginal.delegates.length;i++)
+        {
+            var del2=delOriginal.delegates[i];
+            if(del2 != delToRemove)
+            {
+                if(del.delegates == null)
+                    del.delegates = [];
+                del.delegates.push(del2);
+            }
+        }
+        if(del.delegates == null)
+            return null;
+        if(del.delegates.length == 1)
+            return del.delegates[0];
+        return del;
+    }
+    else
+    {
+        if(delToRemove.isMulticastDelegate)
+            throw new Error("single to multicast delegate removal is not supported");
+        if(delOriginal == delToRemove)
+            return null;
+        return delOriginal;
+    }
+};
 if (typeof($CreateDelegate)=='undefined'){
     if(typeof($iKey)=='undefined') var $iKey = 0;
     if(typeof($pKey)=='undefined') var $pKey = String.fromCharCode(1);
@@ -52,28 +133,24 @@ CoreTests.Events.CustomEvents = function ()
 };
 CoreTests.Events.CustomEvents.prototype.add_MyEvent2 = function (value)
 {
-}
-;
+    this.MyEvent2 = $CombineDelegates(this.MyEvent2, value);
+};
 CoreTests.Events.CustomEvents.prototype.remove_MyEvent2 = function (value)
 {
-}
-;
+    this.MyEvent2 = $RemoveDelegate(this.MyEvent2, value);
+};
 CoreTests.Events.CustomEvents.prototype.MyEvent.add = function (value)
 {
-}
-;
+};
 CoreTests.Events.CustomEvents.prototype.MyEvent.remove = function (value)
 {
-}
-;
+};
 CoreTests.Events.CustomEvents.prototype.add_MyEvent3 = function (value)
 {
-}
-;
+};
 CoreTests.Events.CustomEvents.prototype.remove_MyEvent3 = function (value)
 {
-}
-;
+};
 CoreTests.Events.CustomEventsTest = function ()
 {
 };
